d956f2f42004846e7e5abba504fd037a564065e6
Ryan Bissett <ryan.bissett@scopely.com>
2022-08-19 18:02:33 +0100

    Remove groovy dependency that breaks everything

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,8 +14,6 @@ plugins {
     id("org.jetbrains.changelog") version "1.3.1"
     // Gradle Qodana Plugin
     id("org.jetbrains.qodana") version "0.1.13"
-    // Groovy
-    id("org.jetbrains.groovy") version "0.1.13"
 }
 
 group = properties("pluginGroup")
